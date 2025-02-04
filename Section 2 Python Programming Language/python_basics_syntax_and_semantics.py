349ffb868f88f3ac33d27a205efc9f7271c65cc4
Rohit <rohitchoudhari18n@gmail.com>
2025-02-04 08:58:11 +0530

    Section 2 completed

---
+++ Section 2 Python Programming Language/python_basics_syntax_and_semantics.py
@@ -0,0 +1,30 @@
+# This is a single line comment
+
+# Case Sensitivity- Python is case sensitive
+Name = "Rohit"
+name = "rohit"
+
+print(Name)
+print(name)
+
+# Intendation- Python uses indentation to define code blocks, instead of brackets
+age = 20
+if age > 18:
+    print("You are an adult")
+
+# Line Continuation
+total = 1 + 2 + 3 + 4 + 5
+print(total)
+
+# Variable assignment
+age = 20
+name = "Rohit"
+print(type(age))
+print(type(name))
+
+
+# Type Inference
+variable = 20
+print(type(variable))
+variable = "Rohit"
+print(type(variable))
---
+++ Section 2 Python Programming Language/variables_in_python.py
@@ -0,0 +1,28 @@
+age = 26
+name = "Rohit"
+is_coder = True
+print("Age: ", age)
+print("Name: ", name)
+print("Is Coder: ", is_coder)
+
+
+# Valid variable names
+first_name = "Rohit"
+my_age = 26
+
+# Invalid variable names
+# @first_name = "Rohit"
+# 3age = 26
+
+# Type converstion
+print(type(int(float(5.5))))
+print(type(int(str("5.4"))))
+print(type(str(float(5.5))))
+
+# Dynamic typing
+a = 25
+print(a, type(a))
+a = "Rohit"
+print(a, type(a))
+a = 25.5
+print(a, type(a))
